1f8cef8c3350d2ee09414f1a1f279bdef675c264
Raenore <172234435+Raenore@users.noreply.github.com>
2026-01-15 02:00:13 +0100

    It's a tab life, spaces not allowed

--- Modules/TRP/TRP.lua
+++ Modules/TRP/TRP.lua
@@ -9,15 +9,15 @@ end
 
 local function onStart()
 	TRP3_API.RegisterCallback(TRP3_Addon, TRP3_Addon.Events.WORKFLOW_ON_LOADED, function()
-        if not TRP3_API.toolbar then
-            return;
-        end
+		if not TRP3_API.toolbar then
+			return;
+		end
 
 		TRP3_API.toolbar.toolbarAddButton{
 			id = "trp3_sippy_cup",
 			icon = SIPPYCUP.AddonMetadata.iconTexture,
 			configText = SIPPYCUP.AddonMetadata.title,
-            tooltip = SIPPYCUP.AddonMetadata.title,
+			tooltip = SIPPYCUP.AddonMetadata.title,
 			tooltipSub = L.ADDON_COMPARTMENT_DESC,
 			onClick = function(_, _, button)
 				if button == "LeftButton" then
@@ -32,10 +32,10 @@ end
 
 -- Module Registration
 TRP3_API.module.registerModule({
-    ["name"] = "Sippy Cup",
-    ["description"] = "Adds a toolbar button to open Sippy Cup easily.",
-    ["version"] = SIPPYCUP.AddonMetadata.version,
-    ["id"] = "trp_sippy_cup",
-    ["onStart"] = onStart,
-    ["minVersion"] = 3,
+	["name"] = "Sippy Cup",
+	["description"] = "Adds a toolbar button to open Sippy Cup easily.",
+	["version"] = SIPPYCUP.AddonMetadata.version,
+	["id"] = "trp_sippy_cup",
+	["onStart"] = onStart,
+	["minVersion"] = 3,
 })
